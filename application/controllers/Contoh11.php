545fc063ee1f2b8c96430b05e914fd4986684c34
aksayynef <zillhumgorfins@gmail.com>
2023-12-08 21:42:56 +0700

    P01-P12

---
+++ application/controllers/Contoh11.php
@@ -0,0 +1,14 @@
+<?php
+class Contoh1 extends CI_Controller
+{
+    public function index()
+    {
+        echo "<h1>Perkenalan Diri</h1>";
+        echo "Nama : Aksay Nandez Ergamana Fattah <br>";
+        echo "Nim  : 12220622 <br>";
+        echo "Kelas: 12.3A.05 <br>";
+        echo "TTL  : Bekasi, 23 Juni 2005 <br>";
+        echo "Hobi : Tidur dan Tidur"; 
+        //contoh1.php//
+    }
+}
